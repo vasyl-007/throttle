2a57b28a1afb44ed76209deffc0af80955febfa5
Vasyl-007 <vasyl.manyo@gmail.com>
2020-05-26 19:18:57 +0300

    added throttle function

---
+++ index.js
@@ -0,0 +1,36 @@
+console.log("hello world");
+
+function throttle(func, ms) {
+  let isThrottled = false;
+  let savedArgs;
+  let savedThis;
+
+  function wrapper() {
+    if (isThrottled) {
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+
+    func.apply(this, arguments);
+
+    isThrottled = true;
+
+    setTimeout(function () {
+      isThrottled = false;
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedThis = savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
+}
+
+function mouseMove() {
+  console.log(new Date());
+}
+
+// mouseMove = throttle(mouseMove, 3000);
+// setInterval(mouseMove, 1000)
